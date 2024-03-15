ffeb0492487e2358edd2e831ca8aa10c3ef24d39
PHY Channak <pychannak@gmail.com>
2024-03-15 23:20:02 +0700

    initialize

---
+++ js/menu.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get references to the icons and card menus
+    var menuIcons = document.querySelectorAll('.menu-icon');
+    var cardMenus = document.querySelectorAll('.card-menu');
+
+    // Function to toggle card menu visibility
+    function toggleCardMenu(menuIndex) {
+      if (cardMenus[menuIndex].style.display === 'block') {
+        cardMenus[menuIndex].style.display = 'none';
+      } else {
+        // Hide all other card menus before showing the clicked menu
+        for (var i = 0; i < cardMenus.length; i++) {
+          if (i !== menuIndex) {
+            cardMenus[i].style.display = 'none';
+          }
+        }
+        cardMenus[menuIndex].style.display = 'block';
+      }
+    }
+
+    // Attach click event listeners to each icon
+    menuIcons.forEach(function(icon, index) {
+      icon.addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevent the click event from propagating to the document
+        toggleCardMenu(index);
+      });
+    });
+
+    // Hide card menus when clicking anywhere outside them
+    document.addEventListener('click', function(event) {
+      cardMenus.forEach(function(menu) {
+        if (!menu.contains(event.target)) {
+          menu.style.display = 'none';
+        }
+      });
+    });
+  });
